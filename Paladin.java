5b145f8c8209bec923adff9a3cace726d25d9791
Haixi Zhang <haixi23@bu.edu>
2024-11-17 20:50:55 -0500

    update baseDefense

--- Paladin.java
+++ Paladin.java
@@ -1,7 +1,8 @@
 //Concrete Class to create a Paladin; A concrete class representing a Paladin hero type with unique abilities and attributes.
 public class Paladin extends Hero {
-    public Paladin(String name, int level, int experiencePoints, int mp, int hp, int strength, int dexterity, int agility, int gold) {
-        super(name, level, experiencePoints, mp, hp, strength, dexterity, agility, gold);
+    public Paladin(String name, int level, int experiencePoints, int mp, int hp, int strength, int dexterity,
+            int agility, double baseDefense, int gold) {
+        super(name, level, experiencePoints, mp, hp, strength, dexterity, agility, baseDefense, gold);
     }
 
     @Override
--- Sorcerer.java
+++ Sorcerer.java
@@ -1,7 +1,8 @@
 //Concrete Class to create a Sorcerer
 public class Sorcerer extends Hero {
-    public Sorcerer(String name, int level, int experiencePoints, int mp, int hp, int strength, int dexterity, int agility, int gold) {
-        super(name, level, experiencePoints, mp, hp, strength, dexterity, agility, gold);
+    public Sorcerer(String name, int level, int experiencePoints, int mp, int hp, int strength, int dexterity,
+            int agility, double baseDefense, int gold) {
+        super(name, level, experiencePoints, mp, hp, strength, dexterity, agility, baseDefense, gold);
     }
 
     @Override
